66a4232cf05207ae0a0eea2aade7e3b018136d3e
Andy Sims <andy.sims@zendesk.com>
2022-01-08 13:46:17 +1100

    Fix up .gitignore and add the entry point to repo

---
+++ cmd/conway/conway.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"conway/internal/ui"
+	"conway/internal/universe"
+	"fmt"
+	"os"
+)
+
+const usage = "Usage: conway [file]"
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
+	src := args[1]
+	uni, err := universe.FromFile(src)
+	if err != nil {
+		fmt.Printf("Error reading source file: %v\n", err)
+		os.Exit(1)
+	}
+
+	ui := ui.NewTermUI(uni)
+	ui.Render()
+	os.Exit(0)
+}
